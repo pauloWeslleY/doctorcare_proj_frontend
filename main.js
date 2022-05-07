9c026ad5a3cb0536d243a3f8be87b419b43a1da5
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-05-06 21:30:33 -0300

    nlwReturn missionOrigin aula4 finalizada

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
+window.addEventListener('scroll', onScroll);
+
+onScroll();
 function onScroll() {
+   showNavOnScroll()
+   showBackToTopOnScroll()
+}
+function showNavOnScroll() {
    if (scrollY > 0) {
       navigation.classList.add('scroll');
    } else {
       navigation.classList.remove('scroll');
    }
 }
-
+function showBackToTopOnScroll() {
+   if (scrollY > 550) {
+      btnBackToTop.classList.add('show');
+   } else {
+      btnBackToTop.classList.remove('show');
+   }
+}
 function openMenuMobile() {
    document.body.classList.add('menu__expanded_mobile');
 }
-
 function closeMenuMobile() {
    document.body.classList.remove('menu__expanded_mobile');
 }
-
 //! === === === ===|> ScrollReveal
 ScrollReveal({
    origin: 'top',
