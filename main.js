47a42646292d587ed26217a0e9c27616ae2e6261
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-05-09 21:31:25 -0300

    nlwReturn missionOrigin aula5

--- main.js
+++ main.js
@@ -4,6 +4,35 @@ onScroll();
 function onScroll() {
    showNavOnScroll()
    showBackToTopOnScroll()
+
+   activeMenuAtCurrentSection(home)
+   activeMenuAtCurrentSection(services)
+   activeMenuAtCurrentSection(about)
+   activeMenuAtCurrentSection(contact)
+}
+function activeMenuAtCurrentSection(section) {
+   const targetLine = scrollY + innerHeight / 2;
+
+   //?|> verificar se a seção passou da linha
+   // quais dados vou precisar?
+   const sectionTop = section.offsetTop;
+   const sectionHeight = section.offsetHeight;
+   const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+   // onde a section termina
+   const sectionEndsAt = sectionTop + sectionHeight;
+   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+   //limit
+   const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+   const isSectionId = section.getAttribute('id');
+   const isMenuElement = document.querySelector(`.menu__mobile a[href*=${isSectionId}]`)
+
+   isMenuElement.classList.remove('active')
+   if (sectionBoundaries) {
+      isMenuElement.classList.add('active')
+   }
 }
 function showNavOnScroll() {
    if (scrollY > 0) {
